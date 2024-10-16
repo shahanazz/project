ea2a510dd13a8f90f8ac846002dbd027884f6228
shah <shahanazz193@gmail.com>
2024-10-16 07:46:36 +0530

    wallet history update

--- config/passport.mjs
+++ config/passport.mjs
@@ -8,7 +8,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://elegent.online/auth/google/callback',
+    callbackURL: 'http://localhost:8080/auth/google/callback',
   },
   async (accessToken, tokenSecret, profile, done) => {
     try {
